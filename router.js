89ce9a494bc17cbb7d00cb36f590dace52530b27
Arkadij Kuzhel <arkadyk@netvertise.com>
2014-12-10 17:08:19 +0200

    Return response depending on route

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 function route(handle, pathname) {
     console.log("About to route a request for " + pathname);
+    var ret;
     if (typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        ret = handle[pathname]();
     } else {
         console.log("No request handler found for " + pathname);
+        ret = "404 Not found";
     }
     console.log("");
+    return ret;
 }
 
 exports.route = route;
