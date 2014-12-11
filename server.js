1205a7a4c459b176a7f62d90050ceed8d27d97ff
Arkadij Kuzhel <arkadyk@netvertise.com>
2014-12-11 13:39:36 +0200

    Receive and display image. Handle if no image has been sent.

--- server.js
+++ server.js
@@ -3,23 +3,9 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8");
-
-        var chunkCounter = 0;
-        request.addListener("data", function (postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '" + (++chunkCounter) + "' (" + (postDataChunk.length) + " bytes).");
-        });
-
-        request.addListener("end", function () {
-            route(handle, pathname, response, postData);
-            console.log("Complete receiving POST data - " + postData.length + " bytes.");
-        });
-
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
